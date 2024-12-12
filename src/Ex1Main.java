cb4372c4c880369ab4dd54d65d7214d2aa1e9c91
shira <shira18k@gmail.com>
2024-12-12 21:38:29 +0200

    fixed the main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -19,17 +19,19 @@ public class Ex1Main {
             System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
-            if (!num1.equals("quit")) {
-                System.out.println("num1 = " + num1 + "is number:" + Ex1.isNumber(num1) + " , value:" + Ex1.number2Int(num1));
+            if (num1.equals("quit")) {
+                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
             } else {
-                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");// צריך שיחזור
+                System.out.println("num1 = " + num1 + "is number:" + Ex1.isNumber(num1) + " , value:" + Ex1.number2Int(num1));
+                continue;
             }
             System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
             num2 = sc.next();
-            if (!num2.equals("quit")) {
-                System.out.println("num2 = " + num2 + "is number:" + Ex1.isNumber(num2) + " , value:" + Ex1.number2Int(num2));
-            } else {
+            if (num2.equals("quit")) {
                 System.out.println("ERR: num1 is in the wrong format! (" + num2 + ")");
+            } else {
+                System.out.println("num2 = " + num2 + "is number:" + Ex1.isNumber(num2) + " , value:" + Ex1.number2Int(num2));
+                continue;
             }
             System.out.println("Enter a base for output: (a number [2,16] ");
             int base;
